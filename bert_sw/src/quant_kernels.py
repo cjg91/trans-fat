24810d4ddc54ceaaa996c55709c7f6e7bc0df66c
Colman Glagovich <cjg91@pitt.edu>
2022-03-22 16:59:02 -0400

    Implemented infrastructure for ground truth generation

--- bert_sw/src/quant_kernels.py
+++ bert_sw/src/quant_kernels.py
@@ -1,15 +1,17 @@
 import torch
-from .quant_ops import tensor_quant_scale
 
-def linear_kernel(act, weight_t, bias):
+'''
+These kernels differ from ops because they have well-defined interfaces and operate mainly on integer types.
+These will be mapped, almost exactly, to HLS kernels.
+'''
+
+def linear_kernel(act: torch.CharTensor, weight_t: torch.CharTensor, bias: torch.IntTensor) -> torch.IntTensor:
     '''
-    Linear Layer generalizes to matmul, you just initialize accumulators to zero.
+    Linear
 
     act:        int8 quantized activation
     weight_t:   int8 quantized weights (transposed)
     bias:       int32 quantized bias
-    acc_scale:  scaling factor used for accumulation/bias (act_scale*weight_scale)
-    out_scale:  scaling factor used on output
 
     returns:    int32 quantized output (because it's more general than 8-bit)
     '''
@@ -21,6 +23,37 @@ def linear_kernel(act, weight_t, bias):
     acc = torch.matmul(act, weight_t) + bias
 
     return acc
+
+
+def matmul_kernel(A: torch.CharTensor, B: torch.CharTensor) -> torch.IntTensor:
+    '''
+    Matmul
+
+    A:          int8 quantized A
+    B:          int8 quantized B
+
+    returns:    int32 quantized output (because it's more general than 8-bit)
+    '''
+    
+    # Make sure we're dealing in INTs here
+    A = (A.type(torch.int8)).type(torch.int32)
+    B = (B.type(torch.int8)).type(torch.int32)
+
+    acc = torch.matmul(A, B)
+
+    return acc
+
+
+def requantize_kernel(A: torch.IntTensor, M_scale) -> torch.CharTensor:
+    '''
+    A:      int32 tensor
+    scale:  fixed-point scaling factor to quantize to int8
+    '''
+    bits_out = 8
+
+    A_int = A.float() * M_scale # FixedPoint Multiply
+    A_int = torch.round((torch.clip((A_int), min=-2**(bits_out-1)-1, max=2**(bits_out-1)-1)))
+    return A_int.type(torch.int8)
 
 
 
--- bert_sw/src/quant_layer.py
+++ bert_sw/src/quant_layer.py
@@ -5,43 +5,88 @@ from .quant_ops import (
     tensor_quant_matmul, tensor_quant_linear, tensor_quant_softmax,
     tensor_quant_layernorm, tensor_quant_gelu, tensor_quant_scale
 )
-from .quant_kernels import linear_kernel
+from .quant_kernels import linear_kernel, matmul_kernel, requantize_kernel
 
 def quantize_linear_params(layer, act_scale):
-    weight, weight_scale = tensor_quant_scale(layer.weight)
+    weight, weight_scale = tensor_quant_scale(layer.weight, bits=8)
     acc_scale = weight_scale * act_scale
-    bias, _ = tensor_quant_scale(layer.bias, scale=acc_scale)
+    bias, _ = tensor_quant_scale(layer.bias, scale=acc_scale, bits=32)
 
     return weight, bias, acc_scale
 
-def layer_kernel(layer, hidden_states, attention_mask=None):
+
+def layer_kernel_gt(layer, hidden_states, attention_mask=None, save_gt=True):
     '''
-    Entire encoder layer implemented in basic operations.
+    Entire encoder layer implemented in basic operations. 
+    Treat the output of this layer as ground truth to compare to. 
+    
+    TODO: Update layernorm, softmax, and gelu to take quantized inputs. Verify good performance with before changes are accepted.
     '''
+
+    params = {}
+    stage1_gt = {}
     bs, seqlen, dmodel = hidden_states.size()
     num_heads = layer.attention.self.num_attention_heads
     dhead = layer.attention.self.attention_head_size
+    if save_gt:
+        params['bs'] = bs
+        params['seqlen'] = seqlen
+        params['dmodel'] = dmodel
+        params['num_heads'] = num_heads
+        params['dhead'] = dhead
+        params['hidden_states'] = hidden_states
     
     ###############
     # Stage 1
+    ###############
     ## Quantize
-    _, act_scale = tensor_quant_scale(hidden_states)
+    act_int, act_scale = tensor_quant_scale(hidden_states)
     query_weight, query_bias, query_acc_scale = quantize_linear_params(layer.attention.self.query, act_scale)
     key_weight, key_bias, key_acc_scale = quantize_linear_params(layer.attention.self.key, act_scale)
     value_weight, value_bias, value_acc_scale = quantize_linear_params(layer.attention.self.value, act_scale)
 
-    query_layer = linear_kernel(hidden_states, query_weight.T, query_bias) # <bs, seqlen, dmodel>
-    key_layer = linear_kernel(hidden_states, key_weight.T, key_bias) # <bs, seqlen, dmodel>
-    value_layer = linear_kernel(hidden_states, value_weight.T, value_bias) # <bs, seqlen, dmodel>
+    # linear_kernel returns int32
+    query_layer = linear_kernel(act_int, query_weight.T, query_bias) # <bs, seqlen, dmodel>
+    key_layer = linear_kernel(act_int, key_weight.T, key_bias) # <bs, seqlen, dmodel>
+    value_layer = linear_kernel(act_int, value_weight.T, value_bias) # <bs, seqlen, dmodel>
 
-    ## Dequantize
-    query_layer = query_layer.float() * query_acc_scale
-    key_layer = key_layer.float() * key_acc_scale
-    value_layer = value_layer.float() * value_acc_scale
+    ## Requantize:
+    _, query_out_scale = tensor_quant_scale(query_layer * query_acc_scale)
+    _, key_out_scale = tensor_quant_scale(key_layer * key_acc_scale)
+    _, value_out_scale = tensor_quant_scale(value_layer * value_acc_scale)
+
+    M_query = query_acc_scale / query_out_scale
+    M_key = key_acc_scale / key_out_scale
+    M_value = value_acc_scale / value_out_scale
+
+    # requantize_kernel returns int8
+    query_layer = requantize_kernel(query_layer, M_query)
+    key_layer = requantize_kernel(key_layer, M_key)
+    value_layer = requantize_kernel(value_layer, M_value)
+
+    if save_gt:
+        stage1_gt['act_int'] = act_int
+        stage1_gt['query_weight'] = query_weight
+        stage1_gt['query_bias'] = query_bias
+        stage1_gt['query_acc_scale'] = query_acc_scale
+        stage1_gt['key_weight'] = key_weight
+        stage1_gt['key_bias'] = key_bias
+        stage1_gt['key_acc_scale'] = key_acc_scale
+        stage1_gt['value_weight'] = value_weight
+        stage1_gt['value_bias'] = value_bias
+        stage1_gt['value_acc_scale'] = value_acc_scale
+        stage1_gt['query_out_scale'] = query_out_scale
+        stage1_gt['key_out_scale'] = key_out_scale
+        stage1_gt['value_out_scale'] = value_out_scale
+        stage1_gt['query_layer'] = query_layer
+        stage1_gt['key_layer'] = key_layer
+        stage1_gt['value_layer'] = value_layer
+
+
 
     ###############
     # Stage 2
-    
+    ###############
     new_shape = (bs, seqlen, num_heads, dhead)
     
     query_layer = query_layer.view(new_shape)
@@ -52,36 +97,70 @@ def layer_kernel(layer, hidden_states, attention_mask=None):
     value_layer = value_layer.permute(0,2,1,3) # <bs, num_head, seqlen, dhead>
     key_layer = key_layer.permute(0,2,3,1)     # <bs, num_head, dhead, seqlen>
     
-    attention_scores = tensor_quant_matmul(query_layer, key_layer)
+    attention_scores = matmul_kernel(query_layer, key_layer)
+    
+    # DEQUANT ZONE BEGIN ⬇️`
+    attention_scores = attention_scores.float() * (query_out_scale * key_out_scale)
     attention_scores /= math.sqrt(dhead)
     
     if attention_mask is not None:
         attention_scores = attention_scores + attention_mask
     
     attention_probs = tensor_quant_softmax(attention_scores)
-    # attention_probs = layer.attention.self.dropout(attention_probs)
-    
-    # Weighted sum of Values from softmax attention
-    attention_out = tensor_quant_matmul(attention_probs, value_layer)
+    # DEQUANT ZONE END ^^^^^^^^^^
+    # quantize
+    attention_probs, attention_prob_scale = tensor_quant_scale(attention_probs, bits=8)
+    attention_probs = attention_probs.type(torch.int8)
+
+    attention_out = matmul_kernel(attention_probs, value_layer)
+
+    # requantize
+    _, attention_out_scale = tensor_quant_scale(attention_out*(attention_prob_scale * value_out_scale), bits=8)
+    M_attention_out = attention_prob_scale * value_out_scale / attention_out_scale
+    attention_out = requantize_kernel(attention_out, M_attention_out)
     
     attention_out = attention_out.permute(0,2,1,3).contiguous()
     attention_out = attention_out.view(bs, seqlen, dmodel)
     
-    dense_out = tensor_quant_linear(layer.attention.output.dense, attention_out)
+    dense_weight, dense_bias, dense_acc_scale = quantize_linear_params(layer.attention.output.dense, attention_out_scale)
+    dense_out = linear_kernel(attention_out, dense_weight.T, dense_bias)
+
+    dense_out = dense_out.float() * dense_acc_scale
     
+    # DEQUANT ZONE BEGIN ⬇️`
+
     attention_out = tensor_quant_layernorm(layer.attention.output.LayerNorm, dense_out + hidden_states)
+
+    # DEQUANT ZONE END ^^^^^^^^^^
 
     ###############
     # Stage 3
-    output = tensor_quant_linear(layer.intermediate.dense, attention_out)
+    ###############
+    attention_out_int, attention_out_scale = tensor_quant_scale(attention_out, bits=8)
+    attention_out_int = attention_out_int.type(torch.int8)
+    dense_weight, dense_bias, dense_acc_scale = quantize_linear_params(layer.intermediate.dense, attention_out_scale)
+    output = linear_kernel(attention_out_int, dense_weight.T, dense_bias)
+    
+    # DEQUANT ZONE BEGIN ⬇️`
+    output = output.float() * dense_acc_scale
     output = tensor_quant_gelu(output)
+
+    # DEQUANT ZONE END ^^^^^^^^^^
     
     ###############
     # Stage 4
-    output = tensor_quant_linear(layer.output.dense, output)
-    # output = layer.output.dropout(output)
+    ###############
+
+    output, output_scale = tensor_quant_scale(output, bits=8)
+    output = output.type(torch.int8)
+    dense_weight, dense_bias, dense_acc_scale = quantize_linear_params(layer.output.dense, output_scale)
+    output = linear_kernel(output, dense_weight.T, dense_bias)
+
+    # DEQUANT ZONE BEGIN ⬇️`
+    output = output.float() * dense_acc_scale
+
     output = tensor_quant_layernorm(layer.output.LayerNorm, output + attention_out)
-    
+
     return output
 
 
@@ -99,10 +178,10 @@ def attention(layer, hidden_states, attention_mask=None):
     # Linear transform to get multiple heads. This is a major MAC slurper.
     # Each of these is calling an nn.Linear layer on hidden_states.
 #     query_layer = layer.attention.self.query(hidden_states) # 
-    query_layer = tensor_quant_linear(layer.attention.self.query, hidden_states, out_bits=8) # <bs, seqlen, dmodel>
-    key_layer = tensor_quant_linear(layer.attention.self.key, hidden_states, out_bits=8)     # <bs, seqlen, dmodel>
-    value_layer = tensor_quant_linear(layer.attention.self.value, hidden_states, out_bits=8) # <bs, seqlen, dmodel>
-    
+    query_layer = tensor_quant_linear(layer.attention.self.query, hidden_states) # <bs, seqlen, dmodel>
+    key_layer = tensor_quant_linear(layer.attention.self.key, hidden_states)     # <bs, seqlen, dmodel>
+    value_layer = tensor_quant_linear(layer.attention.self.value, hidden_states) # <bs, seqlen, dmodel>
+
     # Reshape and transpose for multi-head
     new_shape = (bs, seqlen, num_heads, dhead)
     
