dacc62764f4e04b0548b47036f9159a7139c4a60
Daniel <dcs98@pitt.edu>
2022-04-04 16:33:25 -0400

    Added CFG namespace and linear fused for stage1

--- src/stages/pipeline.hpp
+++ src/stages/pipeline.hpp
@@ -1,5 +1,10 @@
-const int seqlen = 128;
-const int nhead = 12;
-const int dhead = 64;
-const int dmodel = 768;
-const int ffdim = 3072;
+namespace CFG
+{
+    const int seqlen = 128;
+    const int nhead = 12;
+    const int dhead = 64;
+    const int dmodel = 768;
+    const int ffdim = 3072;
+} // namespace CONFIG
+
+
