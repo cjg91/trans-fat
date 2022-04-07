f242e9f5474be73176aa6687b1889010ef59f89f
Colman Glagovich <cjg91@pitt.edu>
2022-04-07 12:21:23 -0400

    stage2 prototype

--- src/stages/stage2/stage2.hpp
+++ src/stages/stage2/stage2.hpp
@@ -1,3 +1,3 @@
 #include <cstdint>
 
-void stage2(int8_t* query_in, int8_t* key_in, int8_t* value_in, int8_t* skip_in, int8_t* stage2_out, float M_attention_probs, float M_attention_out, );
+void stage2(int8_t* query_in, int8_t* key_in, int8_t* value_in, int8_t* skip_in, int8_t* stage2_out, float scores_scale, float M_attention_probs, float M_attention_out, float M_dense_out, int16_t* norm_weight, int16_t* norm_bias, float M_stage2);
