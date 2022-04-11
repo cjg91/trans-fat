5f146a9cb8e89adc2056d319fcb0282a43df5610
Daniel <dcs98@pitt.edu>
2022-04-11 19:04:08 -0400

    added changes from PR and tested

--- src/stages/stage4/stage4.cpp
+++ src/stages/stage4/stage4.cpp
@@ -1,5 +1,6 @@
 #include "stage4.hpp"
 #include <math.h>
+#include <stdio.h>
 
 
 void linear_sw(int8_t* A, int8_t* B, int32_t* bias, int32_t* out, const int N, const int M, const int K) {
@@ -53,7 +54,7 @@ void sum(int16_t* in, int16_t* out)
 void diff(int16_t* y, int16_t* act, int16_t* means)
 {
     for (int i = 0; i < CFG::seqlen; ++i){
-        for (int j = 0; j < CFG::dmodel; ++j){
+        for (int j = 0; j < CFG::dmodel; ++j){  
             y[i*CFG::dmodel + j] = act[i*CFG::dmodel + j] - means[i];
         }
     }
@@ -82,39 +83,51 @@ void layernorm_sw(int16_t* act, int16_t* y, int16_t* norm_weight, int16_t* norm_
     for (int i = 0; i < CFG::dmodel * CFG::seqlen; ++i){
         y_sq[i] = int16_t((y[i]*int32_t(y[i]))/CFG::dmodel);
     }
-
+    
     // compute var by summing y^2
     sum(y_sq, var);
-
+    
     // calculate constant for std computation
     int16_t C = int16_t(CFG::eps / scaling_factor);
-
+    
     // compute std
     for (int i = 0; i < CFG::seqlen; ++i){
-        stdev[i] = int16_t(sqrt(var[i] + C));
+        stdev[i] = int16_t(sqrt(float(var[i] + C)));
     }
 
     // perform the division on each element in y
     div(y, stdev);
-
-    // perform el-wise mac
-    for (int i = 0; i < CFG::dmodel * CFG::seqlen; ++i){
-        y[i] = int16_t((y[i] * norm_weight[i] + norm_bias[i])*scaling_factor);
+    
+    // perform macs
+    for (int i = 0; i < CFG::dmodel; ++i){
+        for (int j = 0; j < CFG::seqlen; ++j){
+            y[i*CFG::seqlen + j] = int16_t((y[i*CFG::seqlen + j] * norm_weight[i] + norm_bias[i]) * scaling_factor);
+        }
     }
+
+   delete [] means;
+   delete [] y_sq;
+   delete [] var;
+   delete [] stdev;
     
 }
 
 void stage4_gt(int8_t *fc_in, int8_t *skip_conn, float M_residual, int8_t *dense_weight_t, int32_t *dense_bias, int8_t *dense_out, float M_dense_acc, int16_t *norm_weight, int16_t *norm_bias, float M_stage4)
 {
-    int32_t* fc_out = new int32_t[CFG::seqlen * CFG::ffdim];
-    int8_t* fc_out_quant = new int8_t[CFG::seqlen * CFG::ffdim];
+    int32_t* fc_out = new int32_t[CFG::seqlen * CFG::dmodel];
+    int8_t* fc_out_quant = new int8_t[CFG::seqlen * CFG::dmodel];
     int16_t* ln_fc_in = new int16_t[CFG::seqlen * CFG::dmodel];
     int16_t* ln_out = new int16_t[CFG::seqlen * CFG::dmodel];
 
-    linear_sw(fc_in, dense_weight_t, dense_bias, fc_out, CFG::seqlen, CFG::ffdim, CFG::dmodel);
+    linear_sw(fc_in, dense_weight_t, dense_bias, fc_out, CFG::seqlen, CFG::dmodel, CFG::ffdim);
     requantize(fc_out, fc_out_quant, CFG::seqlen, CFG::dmodel, M_dense_acc);
     add_skip(fc_out_quant, skip_conn, CFG::seqlen * CFG::dmodel);
     requantize(fc_out_quant, ln_fc_in, CFG::seqlen, CFG::dmodel, M_residual);
     layernorm_sw(ln_fc_in, ln_out, norm_weight, norm_bias, M_residual);
     requantize(ln_out, dense_out, CFG::seqlen, CFG::dmodel, M_stage4);
+
+    delete [] fc_out;
+    delete [] fc_out_quant;
+    delete [] ln_fc_in;
+    delete [] ln_out;
 }
--- src/stages/stage4/stage4_test.cpp
+++ src/stages/stage4/stage4_test.cpp
@@ -40,18 +40,23 @@ const bool check(T *A, T *B, const int M, const int N)
 
 int main()
 {
-    // !!!! TODO !!!! Fill in correct sizes
-    int8_t* fc_in = new int8_t[CFG::seqlen];
-    int8_t* skip_conn = new int8_t[CFG::seqlen];
-    int8_t* dense_weight_t = new int8_t[CFG::seqlen];
-    int32_t* dense_bias = new int32_t[CFG::seqlen];
-    int16_t* norm_weight = new int16_t[CFG::seqlen];
-    int16_t* norm_bias = new int16_t[CFG::seqlen];
 
-    // TODO: generate random data for above matricies
+    int8_t* fc_in = new int8_t[CFG::seqlen * CFG::ffdim];
+    int8_t* skip_conn = new int8_t[CFG::seqlen * CFG::dmodel];
+    int8_t* dense_weight_t = new int8_t[CFG::ffdim * CFG::dmodel];
+    int32_t* dense_bias = new int32_t[CFG::dmodel];
+    int16_t* norm_weight = new int16_t[CFG::dmodel];
+    int16_t* norm_bias = new int16_t[CFG::dmodel];
 
-    float M_residual = 0.5;
-    float M_dense_acc = 0.4;
+    genmat(fc_in, CFG::seqlen, CFG::ffdim, 7);
+    genmat(skip_conn, CFG::seqlen, CFG::dmodel, 5);
+    genmat(dense_weight_t, CFG::ffdim, CFG::dmodel, 9);
+    genmat(dense_bias, CFG::dmodel, 1, 44);
+    genmat(norm_weight, CFG::dmodel, 1, 23);
+    genmat(norm_bias, CFG::dmodel, 1, 11);
+
+    float M_residual = 2;
+    float M_dense_acc = 0.04;
     float M_stage4 = 0.3;
 
     int8_t* dense_out_gt = new int8_t[CFG::seqlen * CFG::dmodel];
@@ -61,6 +66,15 @@ int main()
     stage4_gt(fc_in, skip_conn, M_residual, dense_weight_t, dense_bias, dense_out, M_dense_acc, norm_weight, norm_bias, M_stage4);
 
     std::cout << "dense_out: " << (check(dense_out_gt, dense_out, CFG::seqlen, CFG::dmodel) ? "PASSED" : "FAILED") << std::endl;
+
+    delete [] fc_in;
+    delete [] skip_conn;
+    delete [] dense_weight_t;
+    delete [] dense_bias;
+    delete [] norm_weight;
+    delete [] norm_bias;
+    delete [] dense_out;
+    delete [] dense_out_gt;
 
     return 0;
 }
