972f517bb07dfbed893b5a3de5824090ff725209
Colman Glagovich <cjg91@pitt.edu>
2022-04-12 12:23:24 -0400

    attention scores implemented

--- src/stages/stage2/stage2.cpp
+++ src/stages/stage2/stage2.cpp
@@ -80,9 +80,6 @@ void attention_scores(int8_t* query, int8_t* key, int32_t* out, const int seqlen
            }
        }
    }
-
-
-
 }
 
 void stage2_gt(int8_t* query_in, int8_t* key_in, int8_t* value_in, int8_t* skip_in, int8_t* stage2_out, float scores_scale, float M_attention_probs, float M_attention_out, float M_dense_out, int16_t* norm_weight, int16_t* norm_bias, float M_stage2) {
