895a30fa4945f6fee71294fc8e7a411beeea3980
cjg91 <cjg91@viz-n1.crc.pitt.edu>
2022-04-25 12:59:17 -0400

    updated v2 fpga1 to reflect v1 changes

--- src/v2/stages/stage1/stage1.cpp
+++ src/v2/stages/stage1/stage1.cpp
@@ -1,7 +1,7 @@
-#include <sys/types.h>
+#include <inttypes.h>
 #include <iostream>
 #include "config.hpp"
-
+#include "stage1.hpp"
 /*
     A: NxK
     B: KxM
@@ -60,25 +60,66 @@ void stage1_gt(int8_t* in, int8_t* query_out, int8_t* key_out, int8_t* value_out
     out: NxM
     Bias: 1xM
 */
-void linear_fused(int8_t* A, int8_t* B, int32_t* bias, int8_t* out, const int N, const int M, const int K, const float M_scale) {
-    
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            // Initialize accumulator
-            int32_t acc32 = bias[j];
-            for (int k = 0; k < K; k++) {
-                acc32 += int32_t(A[i*K+k] * B[k*M+j]);
+void linear_fused(int8_t* A, int8_t* B, int32_t* bias, int8_t* out, const float M_scale) 
+{
+    // buffers for tile mmult
+    int32_t out_block[TILE_SIZE1][TILE_SIZE1];
+    int8_t B_line[TILE_SIZE1];
+
+    #pragma HLS array_partition dim=2 complete variable=out_block
+    #pragma HLS array_partition dim=1 complete variable=B_line
+
+    for (int it = 0; it < CFG::seqlen/TILE_SIZE1; ++it)
+    {
+        for (int jt = 0; jt < CFG::dmodel/TILE_SIZE1; ++jt)
+        {
+            // initialize output with bias
+            for (int i = 0; i < TILE_SIZE1; ++i){
+                for (int j = 0; j < TILE_SIZE1; ++j){
+                    #pragma HLS unroll
+                    out_block[i][j] = bias[jt*TILE_SIZE1 + j];
+                }
             }
-            out[i * M + j] = int8_t(acc32 * M_scale);
+
+            for (int kt = 0; kt < CFG::dmodel/TILE_SIZE1; ++kt)
+            {
+                for (int k = 0; k < TILE_SIZE1; ++k)
+                {
+                    
+                    // read B values into vector
+                    for (int j = 0; j < TILE_SIZE1; ++j){
+                        B_line[j] = B[(kt * TILE_SIZE1 + k) * CFG::dmodel + jt * TILE_SIZE1 + j];
+                    }
+
+                    for (int i = 0; i < TILE_SIZE1; ++i){
+                        #pragma HLS PIPELINE II=1
+                        int8_t Ai = A[(it * TILE_SIZE1 + i) * CFG::dmodel + kt * TILE_SIZE1 + k];
+                        for (int j = 0; j < TILE_SIZE1; ++j){
+                            #pragma HLS unroll
+                            out_block[i][j] += Ai * B_line[j];
+                            
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < TILE_SIZE1; ++i){
+                #pragma HLS PIPELINE II=1
+                for (int j = 0; j < TILE_SIZE1; ++j){
+                    out[(it * TILE_SIZE1 + i) * CFG::dmodel + jt * TILE_SIZE1 + j] = int8_t(out_block[i][j] * M_scale);
+                }
+            }
+            
         }
+        
     }
 }
 
 extern "C" {
 void stage1(int8_t *in, int8_t *query_out, int8_t *key_out, int8_t *value_out, int8_t *query_weight_t, int32_t *query_bias, int8_t *key_weight_t, int32_t *key_bias, int8_t *value_weight_t, int32_t *value_bias, float M_query, float M_key, float M_value)
-{
-    linear_fused(in, query_weight_t, query_bias, query_out, CFG::seqlen, CFG::dmodel, CFG::dmodel, M_query);
-    linear_fused(in, key_weight_t, key_bias, key_out, CFG::seqlen, CFG::dmodel, CFG::dmodel, M_key);
-    linear_fused(in, value_weight_t, value_bias, value_out, CFG::seqlen, CFG::dmodel, CFG::dmodel, M_value);
+{   
+    linear_fused(in, query_weight_t, query_bias, query_out, M_query);
+    linear_fused(in, key_weight_t, key_bias, key_out, M_key);
+    linear_fused(in, value_weight_t, value_bias, value_out, M_value);
 }
 }
